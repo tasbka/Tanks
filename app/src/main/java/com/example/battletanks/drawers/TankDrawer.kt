dc08a83e208518a1ca17105778a07f4d55bf50c5
tasbka <146024851+tasbka@users.noreply.github.com>
2025-02-01 10:26:39 +0300

    14

---
+++ app/src/main/java/com/example/battletanks/drawers/TankDrawer.kt
@@ -0,0 +1,80 @@
+package com.example.battletanks.drawers
+
+import android.view.View
+import android.widget.FrameLayout
+import com.example.battletanks.CELL_SIZE
+import com.example.battletanks.binding
+import com.example.battletanks.enums.Direction
+import com.example.battletanks.models.Coordinate
+import com.example.battletanks.models.Element
+import com.example.battletanks.utils.checkViewCanMoveThroungBorder
+import com.example.battletanks.utils.getElementByCoordinates
+
+
+class TankDrawer (val container:FrameLayout) {
+    var currentDirection = Direction.UP
+    fun move(myTank: View, direction: Direction, elementsOnContainer: List<Element>) {
+        val layoutParams = myTank.layoutParams as FrameLayout.LayoutParams
+        val currentCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        currentDirection = direction
+        myTank.rotation = direction.rotation
+        when (direction) {
+            Direction.UP -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+            }
+            Direction.DOWN -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+            }
+            Direction.LEFT -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+            }
+            Direction.RIGHT -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+            }
+        }
+
+        val nextCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        if (myTank.checkViewCanMoveThroungBorder(
+                nextCoordinate
+            ) && checkTankCanMoveThrounghMaterial(nextCoordinate, elementsOnContainer)
+        ) {
+            binding.container.removeView(myTank)
+            binding.container.addView(myTank)
+        } else {
+            (myTank.layoutParams as FrameLayout.LayoutParams).topMargin = currentCoordinate.top
+            (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin = currentCoordinate.left
+        }
+    }
+
+
+
+    private fun getTankCoordinates(topLeftCoordinate: Coordinate): List<Coordinate> {
+        val coordinateList = mutableListOf<Coordinate>()
+        coordinateList.add(topLeftCoordinate)
+        coordinateList.add(Coordinate(topLeftCoordinate.top + CELL_SIZE, topLeftCoordinate.left))
+        coordinateList.add(Coordinate(topLeftCoordinate.top, topLeftCoordinate.left + CELL_SIZE))
+        coordinateList.add(
+            Coordinate(
+                topLeftCoordinate.top + CELL_SIZE,
+                topLeftCoordinate.left + CELL_SIZE
+            )
+        )
+        return coordinateList
+    }
+
+    private fun checkTankCanMoveThrounghMaterial(
+        coordinate: Coordinate,
+        elementsOnContainer: List<Element>
+    ): Boolean {
+        getTankCoordinates(coordinate).forEach {
+            val element = getElementByCoordinates(it, elementsOnContainer)
+            if (element != null && !element.material.tankConGoThrough) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+
+}
