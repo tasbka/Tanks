0b948bb4a470e6bf4b985a72e83564d059b539f7
tasbka <146024851+tasbka@users.noreply.github.com>
2025-02-01 10:26:39 +0300

    7

---
+++ app/src/main/java/com/example/battletanks/drawers/GridDrawer.kt
@@ -0,0 +1,52 @@
+package com.example.battletanks.drawers
+
+import android.graphics.Color
+import android.view.View
+import android.widget.FrameLayout
+import com.example.battletanks.CELL_SIZE
+import com.example.battletanks.binding
+
+class GridDrawer(private val context: FrameLayout) {
+    private val allLines = mutableListOf<View>()
+
+    fun removeGrid()
+    {
+        val container = binding.container
+        allLines.forEach{
+            container.removeView(it)
+        }
+    }
+
+    fun drawGrid() {
+        val container = binding.container
+        drawHorizontalLines(container)
+        drawVerticalLines(container)
+    }
+
+    private fun drawHorizontalLines(container: FrameLayout?) {
+        var topMargin = 0
+        while (topMargin <= container!!.height) {
+            val horizontalLine = View(container.context)
+            val layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,1)
+            topMargin += CELL_SIZE
+            layoutParams.topMargin = topMargin
+            horizontalLine.layoutParams = layoutParams
+            horizontalLine.setBackgroundColor(Color.WHITE)
+            allLines.add(horizontalLine)
+            container.addView(horizontalLine)
+        }
+    }
+    private fun drawVerticalLines(container: FrameLayout?) {
+        var leftMargin = 0
+        while (leftMargin <= container!!.width) {
+            val verticalLine = View(container.context)
+            val layoutParams = FrameLayout.LayoutParams(1,  FrameLayout.LayoutParams.MATCH_PARENT)
+            leftMargin += CELL_SIZE
+            layoutParams.leftMargin = leftMargin
+            verticalLine.layoutParams = layoutParams
+            verticalLine.setBackgroundColor(Color.WHITE)
+            allLines.add(verticalLine)
+            container.addView(verticalLine)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/battletanks/drawers/TankDrawer.kt
@@ -0,0 +1,89 @@
+package com.example.battletanks.drawers
+
+import android.view.View
+import android.widget.FrameLayout
+import com.example.battletanks.CELL_SIZE
+import com.example.battletanks.binding
+import com.example.battletanks.enums.Direction
+import com.example.battletanks.models.Coordinate
+import com.example.battletanks.models.Element
+
+class TankDrawer (val container:FrameLayout) {
+    var currentDirection = Direction.UP
+    fun move(myTank: View, direction: Direction, elementsOnContainer: List<Element>) {
+        val layoutParams = myTank.layoutParams as FrameLayout.LayoutParams
+        val currentCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        currentDirection = direction
+        myTank.rotation = direction.rotation
+        when (direction) {
+            Direction.UP -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+            }
+            Direction.DOWN -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+            }
+            Direction.LEFT -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+            }
+            Direction.RIGHT -> {
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+            }
+        }
+
+        val nextCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        if (checkTankCanMoveThrounghBorder(
+                nextCoordinate,
+                myTank
+            ) && checkTankCanMoveThrounghMaterial(nextCoordinate, elementsOnContainer)
+        ) {
+            binding.container.removeView(myTank)
+            binding.container.addView(myTank)
+        } else {
+            (myTank.layoutParams as FrameLayout.LayoutParams).topMargin = currentCoordinate.top
+            (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin = currentCoordinate.left
+        }
+    }
+
+    private fun checkTankCanMoveThrounghBorder(coordinate: Coordinate, myTank: View): Boolean {
+        if (coordinate.top >= 0 &&
+            coordinate.top + myTank.height <= binding.container.height &&
+            coordinate.left >= 0 &&
+            coordinate.left + myTank.width <= binding.container.width
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun getTankCoordinates(topLeftCoordinate: Coordinate): List<Coordinate> {
+        val coordinateList = mutableListOf<Coordinate>()
+        coordinateList.add(topLeftCoordinate)
+        coordinateList.add(Coordinate(topLeftCoordinate.top + CELL_SIZE, topLeftCoordinate.left))
+        coordinateList.add(Coordinate(topLeftCoordinate.top, topLeftCoordinate.left + CELL_SIZE))
+        coordinateList.add(
+            Coordinate(
+                topLeftCoordinate.top + CELL_SIZE,
+                topLeftCoordinate.left + CELL_SIZE
+            )
+        )
+        return coordinateList
+    }
+
+    private fun checkTankCanMoveThrounghMaterial(
+        coordinate: Coordinate,
+        elementsOnContainer: List<Element>
+    ): Boolean {
+        getTankCoordinates(coordinate).forEach {
+            val element = getElementByCoordinates(it, elementsOnContainer)
+            if (element != null && !element.material.tankConGoThrough) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun getElementByCoordinates(
+        coordinate: Coordinate, elementsOnContainer: List<Element>) =
+        elementsOnContainer.firstOrNull { it.coordinate == coordinate }
+
+}
