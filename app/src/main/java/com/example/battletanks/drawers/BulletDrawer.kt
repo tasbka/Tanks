73cc6c3c8c5a25c98943bac42def165fef069275
tasbka <146024851+tasbka@users.noreply.github.com>
2025-02-01 10:26:39 +0300

    8

---
+++ app/src/main/java/com/example/battletanks/drawers/BulletDrawer.kt
@@ -0,0 +1,96 @@
+package com.example.battletanks.drawers
+
+import android.app.Activity
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import com.example.battletanks.CELL_SIZE
+import com.example.battletanks.R
+import com.example.battletanks.enums.Direction
+import com.example.battletanks.models.Coordinate
+import com.example.battletanks.utils.checkViewCanMoveThroungBorder
+
+private const val BULLET_WIDTH = 15
+private const val BULLET_HEIGHT = 15
+
+class BulletDrawer(val container: FrameLayout) {
+
+    fun makeBulletMove(myTank: View, currentDirection: Direction) {
+        Thread ( Runnable {
+            val bullet = createBullet(myTank, currentDirection)
+            while (bullet.checkViewCanMoveThroungBorder(Coordinate(bullet.top, bullet.left))) {
+                when (currentDirection) {
+                    Direction.UP ->(bullet.layoutParams as FrameLayout.LayoutParams).topMargin -= BULLET_HEIGHT
+                    Direction.DOWN ->(bullet.layoutParams as FrameLayout.LayoutParams).topMargin += BULLET_HEIGHT
+                    Direction.LEFT ->(bullet.layoutParams as FrameLayout.LayoutParams).leftMargin -= BULLET_HEIGHT
+                    Direction.RIGHT ->(bullet.layoutParams as FrameLayout.LayoutParams).leftMargin += BULLET_HEIGHT
+                }
+                Thread.sleep(30)
+                (container.context as Activity).runOnUiThread {
+                    container.removeView(bullet)
+                    container.addView(bullet)
+                }
+            }
+            (container.context as Activity).runOnUiThread{
+                container.removeView(bullet)
+            }
+        }).start()
+    }
+    private fun createBullet(myTank: View, currentDirection: Direction): ImageView
+    {
+         return ImageView(container.context)
+            .apply {
+                this.setImageResource(R.drawable.bullet)
+                this.layoutParams = FrameLayout.LayoutParams(BULLET_WIDTH, BULLET_HEIGHT)
+                val bulletCoordinate = getBulletCoordinates(this,myTank,currentDirection)
+                (this.layoutParams as FrameLayout.LayoutParams).topMargin = bulletCoordinate.top
+                (this.layoutParams as FrameLayout.LayoutParams).leftMargin = bulletCoordinate.left
+                this.rotation = currentDirection.rotation
+            }
+    }
+
+    private fun getBulletCoordinates(
+        bullet: ImageView,
+        myTank: View,
+        currentDirection: Direction
+    ): Coordinate{
+        val tankLeftTopCoordinate = Coordinate(myTank.top, myTank.left)
+        when(currentDirection){
+            Direction.UP ->{
+                return Coordinate(
+                    top = tankLeftTopCoordinate.top - bullet.layoutParams.height,
+                    left = getDistanceToMiddleOfTank(
+                        tankLeftTopCoordinate.left, bullet.layoutParams.width)
+                )
+            }
+
+            Direction.DOWN -> {
+                return Coordinate(
+                    top = tankLeftTopCoordinate.top +myTank.layoutParams.height,
+                    left = getDistanceToMiddleOfTank(
+                        tankLeftTopCoordinate.left, bullet.layoutParams.width)
+                )
+            }
+            Direction.LEFT -> {
+                return Coordinate(
+                    top = getDistanceToMiddleOfTank(
+                        tankLeftTopCoordinate.top, bullet.layoutParams.height),
+                    left = tankLeftTopCoordinate.left - bullet.layoutParams.width,
+                )
+            }
+            Direction.RIGHT -> {
+                return Coordinate(
+                    top = getDistanceToMiddleOfTank(
+                        tankLeftTopCoordinate.top, bullet.layoutParams.height),
+                    left = tankLeftTopCoordinate.left + myTank.layoutParams.width,
+                )
+            }
+        }
+        return tankLeftTopCoordinate
+    }
+
+    private fun getDistanceToMiddleOfTank(startCoordinate: Int, bulletSize: Int): Int{
+
+        return startCoordinate+(CELL_SIZE - bulletSize/2)
+    }
+}
